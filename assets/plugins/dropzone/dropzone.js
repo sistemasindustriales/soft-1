a8b69966297e29f9eba309f47198e0143eb29a3f
erpsur <84876461+erpsur@users.noreply.github.com>
2021-12-05 20:42:21 -0300

    Update dropzone.js

--- assets/plugins/dropzone/dropzone.js
+++ assets/plugins/dropzone/dropzone.js
@@ -268,7 +268,7 @@
             node.innerHTML = this.filesize(file.size);
           }
           if (this.options.addRemoveLinks) {
-            file._removeLink = Dropzone.createElement("<a class=\"dz-remove\" href=\"javascript:undefined;\" data-dz-remove>" + this.options.dictRemoveFile + "</a>");
+            file._removeLink = Dropzone.createElement("<a class=\"dz-remove\" href=\"javascript:undefined;\" data-dz-remove>" Quitar Archivo "</a>");
             file.previewElement.appendChild(file._removeLink);
           }
           removeFileEvent = (function(_this) {
